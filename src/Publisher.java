8ee4503eb109434f93e35cc9ac8ac3d50fcf421d
Julia Miranda Rodrigues <juliamr@id.uff.br>
2023-05-21 20:03:15 -0300

    exclui funções e atributo não utilizados

--- src/Publisher.java
+++ src/Publisher.java
@@ -7,12 +7,10 @@ import java.util.Map;
 
 public class Publisher extends UnicastRemoteObject implements PubSubInterface {
     private Map<String, List<SubscriberInterface>> subscribersByTopic;
-    private Map<String, String> topicData;
 
     public Publisher() throws RemoteException {
         super();
         subscribersByTopic = new HashMap<>();
-        topicData = new HashMap<>();
     }
 
     @Override
@@ -20,13 +18,11 @@ public class Publisher extends UnicastRemoteObject implements PubSubInterface {
         subscribeToTopic("default", subscriber);
     }
 
-
     @Override
     public void publish(String message) throws RemoteException {
         System.out.println("Publicando mensagem: " + message);
         String[] parts = message.split(":");
-        String topic = parts[0];
-     
+        String topic = parts[0];   
 
         if (subscribersByTopic.containsKey(topic)) {
             List<SubscriberInterface> subscribers = subscribersByTopic.get(topic);
@@ -34,8 +30,6 @@ public class Publisher extends UnicastRemoteObject implements PubSubInterface {
                 subscriber.update(message);
             }
         }
-
-
     }
 
     @Override
@@ -46,27 +40,4 @@ public class Publisher extends UnicastRemoteObject implements PubSubInterface {
         subscribersByTopic.put(topic, subscribers);
         System.out.println("Assinante " + subscriber.getName() + " inscrito no tópico: " + topic);
     }
-
-   
-    // Método para criar informações para um tópico específico
-    public synchronized void createTopicData(String topic, String data) {
-        topicData.put(topic, data);
-        System.out.println("Informações criadas para o tópico " + topic + ": " + data);
-    }
-
-    // Método para publicar informações para um tópico específico
-    
-    public synchronized void publishTopicData(String topic) throws RemoteException {
-        if (subscribersByTopic.containsKey(topic) && topicData.containsKey(topic)) {
-            List<SubscriberInterface> subscribers = subscribersByTopic.get(topic);
-            String data = topicData.get(topic);
-            for (SubscriberInterface subscriber : subscribers) {
-                subscriber.update(data);
-            }
-        }
-    }
-    
-
-    
-
 }
